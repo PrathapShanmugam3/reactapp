3dac19591b1dfd6f479874f9047ce14c7e588b27
prathapshanmugam5 <prathapshanmugam5@gmail.com>
2025-10-30 09:53:12 +0000

    project changed

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjpCNLRgPmIWHcf7JxkpR601ZNch_Q6VQ",
+  apiKey: "AIzaSyATWJJpxYOg8WOddyRB-ppYhRra3lZeehg",
   authDomain: "datacode-87922.firebaseapp.com",
   databaseURL: "https://datacode-87922-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "datacode-87922",
   storageBucket: "datacode-87922.appspot.com",
   messagingSenderId: "460766907792",
-  appId: "1:460766907792:web:c157833c4b24d7f2a0b715",
-  measurementId: "G-5JXHRKS1H0"
+  appId: "1:460766907792:web:a4c633957360f1c6a0b715",
+  measurementId: "G-SKXTPJLLJW"
 };
 
 const app = initializeApp(firebaseConfig);
